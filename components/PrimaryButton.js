ced9cd2c248812729d1ac6961b65929f7ae952ac
Adnan Wazwaz <adnanwazwaz437@gmail.com>
2025-04-24 08:32:21 -0500

    Basic design startgamescreen

---
+++ components/PrimaryButton.js
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+
+export default function PrimaryButton({ children, onPress }) {
+
+    return (
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                style={({ pressed }) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}
+                onPress={onPress}
+                android_ripple={{ color: '#640233' }}
+            >
+                <Text style={styles.buttonText}>
+                    {children}
+                </Text>
+            </Pressable>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
+        borderRadius: 28,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+        margin: 4
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75
+    }
+});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,84 @@
+import { StyleSheet, TextInput, View, Alert } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+import { useState } from 'react';
+/**
+ * Note: autocapitalization not needed for number fields.
+ * 
+ * @returns 
+ */
+export default function StartGameScreen() {
+    const [enteredNumber, setEnteredNumber] = useState('');
+
+    function numberInputHandler(enteredText) {
+        setEnteredNumber(enteredText);
+    }
+
+    function resetInputHandler() {
+        setEnteredNumber('');
+    }
+
+    function confirmInputHandler() {
+        const chosenNumber = parseInt(enteredNumber);
+
+        if (isNaN(chosenNumber) || (chosenNumber <= 0 && chosenNumber > 99)) {
+            // show alert...
+            Alert.alert('Invalid number!', 'Number must be an integer between 1 and 99.', [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }])
+            return;
+        }
+    }
+
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType='number-pad'
+                autoCapitalize='none'
+                autoCorrect={false}
+                value={enteredNumber}
+                onChangeText={numberInputHandler}
+            />
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        alignItems: 'center',
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: '#3b021f',
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    buttonsContainer: {
+        flexDirection: 'row'
+    },
+    buttonContainer: {
+        flex: 1
+    }
+})
